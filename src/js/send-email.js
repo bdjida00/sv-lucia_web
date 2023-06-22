3817c572ff98edd88252042e070d02dce3f5e3d2
Bruno <bruno.djidara@gmail.com>
2023-06-22 22:43:26 +0200

    ne radi kontakt forma

--- src/js/send-email.js
+++ src/js/send-email.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       // Send an AJAX request to the server
       $.ajax({
         type: 'POST',
-        url: 'php/send-email.php',
+        url: 'send-email.php',
         data: formData,
         success: function(response) {
           // Clear the form
